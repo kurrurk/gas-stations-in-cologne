f7ea3c28fdac8d7d7c08c615506cb97e4a7bcc64
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-01-13 09:04:43 +0100

    010. Add REST endpoint to generate HTML for search and sorting results

--- blocks/class.gas-stations-list-block.php
+++ blocks/class.gas-stations-list-block.php
@@ -11,7 +11,7 @@ if (! class_exists('Gas_Stations_Block_List')) {
 		public function __construct()
 		{
 			add_action('init', array($this, 'create_block_gas_stations_in_cologne_block_init'));
-			// add_action('rest_api_init', array($this, 'gas_stations_rest_api_init'));
+			add_action('rest_api_init', array($this, 'gas_stations_rest_api_init'));
 
 			add_action('enqueue_block_assets', array($this, 'enqueue_block_assets'), 999);
 		}
@@ -63,11 +63,68 @@ if (! class_exists('Gas_Stations_Block_List')) {
 			wp_enqueue_style('gas-stations-bootstrap-css');
 		}
 
-		// public function gas_stations_rest_api_init() {
-		// 	register_rest_route('gas-stations/v1', '/filter', [
-		// 		'methods'  => 'GET',
-		// 		'callback' => 'gas_stations_rest_filter',
-		// 	]);
-		// }
+		public function gas_stations_rest_api_init()
+		{
+			register_rest_route('gas-stations/v1', '/filter', [
+				'methods'  => 'GET',
+				'callback' =>  array($this, 'gas_stations_rest_filter'),
+				'permission_callback' => '__return_true'
+			]);
+		}
+
+		public function gas_stations_rest_filter(WP_REST_Request $request)
+		{
+
+			$search     = sanitize_text_field($request->get_param('search'));
+			$sort_by    = sanitize_text_field($request->get_param('sortBy'));
+			$sort_order = sanitize_text_field($request->get_param('sortOrder'));
+			$columns    = (int) $request->get_param('columns');
+
+			$args = [
+				'post_type'      => 'gas-station',
+				'posts_per_page' => -1,
+				'meta_query'     => [],
+			];
+
+			if ($search) {
+				$args['meta_query'][] = [
+					'key'     => 'gas-station_address',
+					'value'   => $search,
+					'compare' => 'LIKE',
+				];
+			}
+
+			if ($sort_by === 'id') {
+				$args['orderby']  = 'meta_value_num';
+				$args['meta_key'] = 'gas-station_object_id';
+			} else {
+				$args['orderby']  = 'meta_value';
+				$args['meta_key'] = 'gas-station_address';
+			}
+
+			$args['order'] = strtoupper($sort_order) === 'DESC' ? 'DESC' : 'ASC';
+
+			$query = new WP_Query($args);
+
+			ob_start();
+
+			if ($query->have_posts()) {
+				while ($query->have_posts()) {
+					$query->the_post();
+
+					$address = get_post_meta(get_the_ID(), 'gas-station_address', true);
+					$x = get_post_meta(get_the_ID(), 'gas-station_geometry_x', true);
+					$y = get_post_meta(get_the_ID(), 'gas-station_geometry_y', true);
+
+					require(GAS_STATIONS_PATH .  'views/gas_stations_card.php');
+				}
+			} else {
+				echo '<p>No results found.</p>';
+			}
+
+			wp_reset_postdata();
+
+			return ob_get_clean();
+		}
 	}
 }
